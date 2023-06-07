db4bd1bdea85211dca2a4c426fd45548357f4bc1
John Christian B. Narbaja <narbajajc@gmail.com>
2023-06-07 22:25:30 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1>This is Home page</h1>
+  </main>
+</template>
---
+++ src/views/Students/StudentsView.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <div class="card">
+        <div class="card-header">
+          <h4>Students
+            <RouterLink to="/create/student" class="btn btn-primary">Add Student</RouterLink>
+          </h4>
+        </div>
+
+        <div class="card-body">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Course</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Created At</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody v-if="students.length > 0">
+              <tr v-for="(student, index) in students" :key="index">
+                <td>{{ student.id }}</td>
+                <td>{{ student.name }}</td>
+                <td>{{ student.course }}</td>
+                <td>{{ student.email }}</td>
+                <td>{{ student.phone }}</td>
+                <td>{{ student.created_at }}</td>
+
+                <td>
+                  <RouterLink to="/create/student" class="btn btn-success">
+                    Edit
+                  </RouterLink>
+
+                  <button type="button" class="btn btn-danger">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-else>
+              <td colspan="7">Loading...</td>
+            </tbody>
+          </table>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'students',
+  data() {
+    return{
+      students: []
+    }
+  },
+  mounted(){
+    this.getStudents();
+    // console.log('Success')
+    
+  },
+  methods: {
+    getStudents(){
+      axios.get('http://localhost:8000/api/students')
+      .then(res => {
+      this.students = res.data.students;
+      });
+
+    }
+  },
+}
+</script>
